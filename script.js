b8975fecc82558d544513ce0040a866b8dbe1e77
havicke <victorfrancis612@gmail.com>
2023-06-07 06:52:12 +0100

    add functionality for displaying score for both computer and player and also displayfinal winner of round

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 'use strict';
+let playersChoice;
+
+let playerScore = 0;
+let computerScore = 0;
+
 
 const getComputerChoice = () => {
   //functions randomly returns either rock or paper or scissors
@@ -10,41 +15,63 @@ const getComputerChoice = () => {
 };
 
 const playRound = (playerSelection, computerSelection) => {
-  let playersChoice = prompt('Rock Paper Scissors').toLowerCase();
   playerSelection = playersChoice;
   computerSelection = getComputerChoice();
   if (playerSelection === computerSelection) {
+    
     return "It's a Tie, Keep Playing";
+
   }
   //instances of player losing
   else if (
     (playerSelection === 'scissors' && computerSelection === 'rock') ||
     (playerSelection === 'paper' && computerSelection === 'scissors') ||
     (playerSelection === 'rock' && computerSelection === 'paper')
-  ) {
+  ) {computerScore++
+     console.log(
+       `You Lose---- Your Score ${playerScore}:${computerScore} Computer Score`
+     );
     return 'You Lose!';
   } else if (
     (computerSelection === 'scissors' && playerSelection === 'rock') ||
     (computerSelection === 'paper' && playerSelection === 'scissors') ||
     (computerSelection === 'rock' && playerSelection === 'paper')
-  ) {
+
+  ) { playerScore++
+     console.log(
+      `You Win---- Your Score ${playerScore}:${computerScore} Computer Score`
+    );
     return 'You Win!';
+  
   } else {
     return 'Please select a Correct Name';
   }
 };
 
-
 const game = () => {
- let playerScore = 0;
-  let computerScore = 0; 
-for (let game=0; game<5;game++){
- console.log(playRound());
-}
-  // } else if (playRound() === 'You Lose!') {
-  //   return (computerScore = computerScore + 1);
-  // }
-  // console.log(playerScore);
-  // console.log(computerScore);
+  for (let game = 0; game < 5; game++) {
+    playersChoice = prompt('Rock Paper Scissors').toLowerCase();
+    console.log(playRound());
+  }
 };
 game();
+const displayScoreMessage = () => {
+  if (playerScore > computerScore) {
+    if (computerScore<playerScore) {
+      console.log(
+        `You Win This Round---- Your Score ${playerScore}:${computerScore} Computer Score`
+      );
+    }
+  }
+  if (playerScore < computerScore) {
+    console.log(
+      `You Lose This Round---- Your Score ${playerScore}:${computerScore} Computer Score`
+    );
+  }
+  if ((playerScore === computerScore)) {
+    console.log(
+      `You Tie This Round---- Your Score ${playerScore}:${computerScore} Computer Score`
+    );
+  }
+};
+displayScoreMessage();
